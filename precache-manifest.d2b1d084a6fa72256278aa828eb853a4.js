2afacd6f0137e78d28be9ac8d1392ecc55908b90
Natalia Chrząścik <56516256+nataliachrzascik@users.noreply.github.com>
2020-05-12 11:53:51 +0200

    Add files via upload

---
+++ precache-manifest.d2b1d084a6fa72256278aa828eb853a4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7864c755afca6ff185395e96952e1e",
+    "url": "/-quarantineApp/index.html"
+  },
+  {
+    "revision": "e5ff0711a625f969e3bb",
+    "url": "/-quarantineApp/static/css/main.a4a547cf.chunk.css"
+  },
+  {
+    "revision": "138385b8fce4b4e0d4a4",
+    "url": "/-quarantineApp/static/js/2.11b07fd0.chunk.js"
+  },
+  {
+    "revision": "b846b5b2fcb30d4eb0a413c317540089",
+    "url": "/-quarantineApp/static/js/2.11b07fd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5ff0711a625f969e3bb",
+    "url": "/-quarantineApp/static/js/main.f14dc1d4.chunk.js"
+  },
+  {
+    "revision": "2cbdacf0b803d4434499",
+    "url": "/-quarantineApp/static/js/runtime-main.56069b50.js"
+  },
+  {
+    "revision": "b9d9b1ed3f024e566149e83885aaca0d",
+    "url": "/-quarantineApp/static/media/1.b9d9b1ed.jpg"
+  },
+  {
+    "revision": "a7105eddfbb296f7a1e8e7b268596c31",
+    "url": "/-quarantineApp/static/media/books.a7105edd.jpg"
+  },
+  {
+    "revision": "bc67cb8b806bcd9e17a084ab745cab53",
+    "url": "/-quarantineApp/static/media/good.bc67cb8b.jpg"
+  },
+  {
+    "revision": "608b837289c197c7fc186ea99203075f",
+    "url": "/-quarantineApp/static/media/star.608b8372.jpg"
+  },
+  {
+    "revision": "e769a1f08d9572570de7f5bdc147ebf1",
+    "url": "/-quarantineApp/static/media/ticket.e769a1f0.jpg"
+  }
+]);
